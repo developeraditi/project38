c9899b0cdf14c08446182bb69088542e2addab50
developeraditi <aditibsrivastava@gmail.com>
2024-02-12 23:17:55 +0530

    Detail View Code

--- app/models.py
+++ app/models.py
@@ -1,15 +1,23 @@
 from django.db import models
 
+from django.urls import reverse
 # Create your models here.
 class School(models.Model):
     sname=models.CharField(max_length=100)
     sprincipal=models.CharField(max_length=100)
     slocation=models.CharField(max_length=100)
+    def get_absolute_url(self):
+        return reverse('details',kwargs={'pk':self.pk}) 
 
     def __str__(self):
         return self.sname
+
+
 
 class Student(models.Model):
     stname =models.CharField(max_length=100)
     sage =models.IntegerField()
     sname=models.ForeignKey(School,on_delete=models.CASCADE,related_name='students')
+
+    
+    
--- app/views.py
+++ app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView
+from django.views.generic import ListView,DetailView,CreateView,UpdateView
 from app.models import *
 
 class SchoolList(ListView):
@@ -10,3 +10,15 @@ class SchoolList(ListView):
     ordering=['sname']
     #queryset =School.objects.filter(id=1)
     #template_name='app/school_list.html'
+
+class SchoolDetail(DetailView):
+    model =School
+    context_object_name='schlobject'
+
+#class SchoolCreateView(CreateView):
+   # model = School
+    #fields ='__all__'
+
+#class SchoolUpdate(UpdateView):
+    ##model = School
+   # fields ='__all__'
